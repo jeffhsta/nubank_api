cc333eef20197186dfb6ac06ca7995820628d52c
Jefferson Stachelski <jefferson.stachelski@gmail.com>
2019-02-11 01:00:01 -0200

    Change the test to check NubankAPI functions

--- test/nubank_api_test.exs
+++ test/nubank_api_test.exs
@@ -1,8 +1,12 @@
 defmodule NubankAPITest do
   use ExUnit.Case
-  doctest NubankAPI
 
-  test "greets the world" do
-    assert NubankAPI.hello() == :world
+  test "NubankAPI has a function transactions/1" do
+    functions =
+      :functions
+      |> NubankAPI.__info__()
+      |> Enum.map(fn {func_name, func_args_count} -> "#{func_name}/#{func_args_count}" end)
+
+    assert "transactions/1" in functions
   end
 end
