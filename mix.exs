8eb386c1495b88c53005a5fc6bf0f133a7166bde
Jefferson Stachelski <jefferson.stachelski@gmail.com>
2019-02-10 23:33:00 -0200

    Add license file and project descriptions in mix file

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule NubankAPI.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package()
     ]
   end
 
@@ -18,6 +20,18 @@ defmodule NubankAPI.MixProject do
   end
 
   defp deps do
-    []
+    [
+      {:ex_doc, ">= 0.0.0", only: :dev}
+    ]
+  end
+
+  defp description, do: "Nubank API client implementation"
+
+  defp package do
+    [
+      maintainers: ["Jefferson Stachelski"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/jeffhsta/nubank_api"}
+    ]
   end
 end
