850d9941302b1bcefc08dd9036c3c21fc88e53fb
Jefferson Stachelski <jefferson.stachelski@gmail.com>
2019-02-19 17:50:41 -0300

    Major version up to 1.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule NubankAPI.MixProject do
   def project do
     [
       app: :nubank_api,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
